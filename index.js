08e5f9a28ddc11f69a8261169e387fd7aafc18e7
Mark De Vera <mdevera_@live.com>
2023-01-06 10:49:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function roll() {
     let randomNum1 = Math.floor(Math.random() * 100) + 1; // 1-100
     document.querySelector("h2").innerHTML = randomNum1
 }
+
+// detecting keyboard press
+
+document.addEventListener('keydown', keypress)
+
+// making only spacebar roll
+//console.log(KeyboardEvent) to see what event triggered the event listener
+
+function keypress(KeyboardEvent) {
+    console.log(KeyboardEvent)
+    if(KeyboardEvent.key === ' '){
+        roll()
+    }
+}
